0bdb139c4bdbe63131ff054a4d29d72491c549a8
占益民 <allen@peytondeimac.local>
2018-05-17 16:21:14 +0800

    Initial Commit

---
+++ Wave/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wave
+//
+//  Created by Peyton on 2018/5/17.
+//  Copyright © 2018年 Peyton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Wave/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Wave
+//
+//  Created by Peyton on 2018/5/17.
+//  Copyright © 2018年 Peyton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
